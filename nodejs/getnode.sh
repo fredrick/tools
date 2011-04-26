62b7eb79ee5e2e44b7f16b6ce6e2924f797da7c9
Fredrick Galoso <fred@wayoutmind.com>
2011-04-26 01:08:35 -0500

    added node.js install script

---
+++ nodejs/getnode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script will download and install the latest node.js + npm
+# Install node.js
+git clone https://github.com/joyent/node.git
+cd node
+mkdir ~/local
+./configure --prefix=$HOME/local/node
+make
+make install
+export PATH=$HOME/local/node/bin:$PATH
+rm -rvf node/
+# Install NPM
+curl http://npmjs.org/install.sh | sh
