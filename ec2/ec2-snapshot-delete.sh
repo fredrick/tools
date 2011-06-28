1e32f0b01d69761d4146e1169edcfc91551ac5bc
Fredrick Galoso <fred@wayoutmind.com>
2011-06-28 16:41:26 -0500

    added basic usage

--- ec2/ec2-snapshot-delete.sh
+++ ec2/ec2-snapshot-delete.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# ec2-snapshot-delete: Deletes the oldest N-th snapshots
+# Example (delete the 200th oldest snapshots): ec2-snapshot-delete 200
 ec2-describe-snapshots | sort -k 5 | head -$1 | awk '{print "Deleting: " $2; system("ec2-delete-snapshot " $2)}'
