51a3849e9644d69b56c01d8414a21b774f13eaac
Fredrick Galoso <fred@wayoutmind.com>
2011-06-28 16:37:15 -0500

    output : nitpick

--- ec2/ec2-snapshot-delete.sh
+++ ec2/ec2-snapshot-delete.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ec2-describe-snapshots | sort -k 5 | head -$1 | awk '{print "Deleting-> " $2; system("ec2-delete-snapshot " $2)}'
+ec2-describe-snapshots | sort -k 5 | head -$1 | awk '{print "Deleting: " $2; system("ec2-delete-snapshot " $2)}'
