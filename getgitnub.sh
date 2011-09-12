7bc2d893d5a98292e14895e02b5651c8eed81cf1
Fredrick Galoso <fred@wayoutmind.com>
2011-09-12 10:00:00 -0500

    make gitnub recipe use git://

--- getgitnub.sh
+++ getgitnub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script will download and install the latest GitNub
 # Fetch GitNub
-git clone https://github.com/Caged/gitnub.git
+git clone git://github.com/Caged/gitnub.git
 cd gitnub
 # Install dependencies
 git submodule init
